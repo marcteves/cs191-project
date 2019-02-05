94bbbde13f565f1b10ed0866818447e9ed09bea1
Hakurei Reimu <gmike.travis@gmail.com>
2019-02-05 12:00:47 +0800

    barebones login action for Sessions#create

--- iskolivery/app/controllers/sessions_controller.rb
+++ iskolivery/app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+	  user = User.find_by(email: params[:email])
+	  if user && user.authenticate(params[:password])
+		  # Log-in
+		  log_in user # find this in SessionsHelper
+	  else
+		  # Invalid info
+		  # TODO: create error
+		  redirect_to login_path
+	  end
   end
 
   def destroy
   end
+
 end
