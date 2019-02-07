c6e13c15e3729a3e3d8203e5f6b5a7d9eff582eb
Hakurei Reimu <gmike.travis@gmail.com>
2019-02-08 00:40:48 +0800

    added create request action

--- iskolivery/app/controllers/requests_controller.rb
+++ iskolivery/app/controllers/requests_controller.rb
@@ -3,5 +3,16 @@ class RequestsController < ApplicationController
   end
 
   def create
+	  user = @current_user
+	  location = Location.find_by(params[:location])
+
+	  request = Request.create(
+		  title: params[:title],
+		  location_id: location.id,
+		  item_name: params[:item_name]
+	  )
+
+	  user.postings.create(request_id: request.id)
+	  redirect_to 'home'
   end
 end
