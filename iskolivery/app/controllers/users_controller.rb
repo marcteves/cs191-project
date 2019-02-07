c8144b3b3541543af0514abf5b6d763aa64eff63
Hakurei Reimu <gmike.travis@gmail.com>
2019-02-07 19:11:46 +0800

    added view accepted requests action for User

--- iskolivery/app/controllers/users_controller.rb
+++ iskolivery/app/controllers/users_controller.rb
@@ -2,12 +2,22 @@ class UsersController < ApplicationController
 
 	# render User homepage
 	def home
-		@user = @current_user 
+		user = @current_user 
 
-		# show all requests not posted by and not accepted by user
+		# get all requests not posted by and not accepted by user
 		# keep in mind null values
 		@available_requests = Requests.where(fulfiller: nil).
-			or(Requests.where.not(fulfiller: @user.id)).
-			where.not(requester: @user.id)
+			or(Requests.where.not(fulfiller: user.id)).
+			where.not(requester: user.id)
+	end
+
+	# render accepted requests page
+	# this could be posted requests page as well
+	def my_requests
+		user = @current_user
+
+		# get all requests accepted by user
+		# in the future, edit this to filter out fulfilled (completed)
+		@accepted_requests = Requests.where(fulfiller: user.id)
 	end
 end
