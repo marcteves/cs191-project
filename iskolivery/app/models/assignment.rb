8ec4cc9be11ef5ff3e528478e9fcd62dde18f6db
Hakurei Reimu <gmike.travis@gmail.com>
2019-02-08 00:38:47 +0800

    made fulfiller association optional

--- iskolivery/app/models/assignment.rb
+++ iskolivery/app/models/assignment.rb
@@ -1,9 +1,11 @@
 class Assignment < ApplicationRecord
 	belongs_to :requester, class_name: "User", foreign_key: "requester_id"
-	belongs_to :fulfiller, class_name: "User", foreign_key: "fulfiller_id"
+	
+	belongs_to :fulfiller, class_name: "User", foreign_key: "fulfiller_id",
+		optional: true # since we can choose not to include this
 	belongs_to :request
 
 	# Only validate requester
 	# fulfiller can be null at creation time
-	validates :requester, presence: true
+	validates :requester_id, presence: true
 end
