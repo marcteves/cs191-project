2d614fdd60fbbd6723b08656b53deae840b987c5
Hakurei Reimu <gmike.travis@gmail.com>
2019-02-08 01:27:04 +0800

    added forward-slash to route paths

--- iskolivery/config/routes.rb
+++ iskolivery/config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 		'home',			to:	'users#home' # user homepage
-  get 		'my_requests', 	to: 'users#my_requests' # user accepted requests
-  post		'accept',		to: 'users#accept_request' # accept a request
+  get 		'/home',		to:	'users#home' # user homepage
+  get 		'/my_requests', to: 'users#my_requests' # user accepted requests
+  post		'/accept',		to: 'users#accept_request' # accept a request
 
   resources :requests
 
-  get 		'login', 	to: 'sessions#new'
-  post 		'login', 	to: 'sessions#create'
-  delete 	'login', 	to: 'sessions#destroy'
+  get 		'/login', 	to: 'sessions#new'
+  post 		'/login', 	to: 'sessions#create'
+  delete 	'/login', 	to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
