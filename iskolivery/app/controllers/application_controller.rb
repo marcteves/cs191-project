d4fa7ae0f654bf63b7e61c781cca234bac808cba
Hakurei Reimu <gmike.travis@gmail.com>
2019-02-15 16:09:25 +0800

    fixed broken licenses

--- iskolivery/app/controllers/application_controller.rb
+++ iskolivery/app/controllers/application_controller.rb
@@ -1,17 +1,22 @@
-<!-- LICENSE -->
-<!--This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering, University of the Philippines, Diliman for the AY 2018-2019
-
-This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
--->
-<!-- Author: Marc Teves-->
-
-<!-- CODE HISTORY -->
-
-<!-- INFORMATION -->
-<!-- File creation: February 5, 2019 -->
-<!-- Development group: Group 1 - Iskolivery -->
-<!-- Client group: None -->
-<!-- Purpose of the software: To create a crowdsourced courier service for the UP Community -->
+# LICENSE
+# This is a course requirement for CS 192 Software Engineering II under the 
+# supervision of Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer 
+# Science, College of Engineering, University of the Philippines, Diliman for 
+# the AY 2018-2019
+# 
+# This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 
+# International License.
+# 
+# Author: Marc Teves
+# 
+# CODE HISTORY
+# 
+# INFORMATION
+# File creation: February 5, 2019
+# Development group: Group 1 - Iskolivery
+# Client group: None
+# Purpose of the software: To create a crowdsourced courier service for 
+# the UP Community
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
--- iskolivery/db/seeds.rb
+++ iskolivery/db/seeds.rb
@@ -1,24 +1,31 @@
-<!-- LICENSE -->
-<!--This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering, University of the Philippines, Diliman for the AY 2018-2019
-
-This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
--->
-<!-- Author: Marc Teves-->
-
-<!-- CODE HISTORY -->
-
-<!-- INFORMATION -->
-<!-- File creation: February 5, 2019 -->
-<!-- Development group: Group 1 - Iskolivery -->
-<!-- Client group: None -->
-<!-- Purpose of the software: To create a crowdsourced courier service for the UP Community -->
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# LICENSE
+# This is a course requirement for CS 192 Software Engineering II under the 
+# supervision of Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer 
+# Science, College of Engineering, University of the Philippines, Diliman for the 
+# AY 2018-2019
+# 
+# This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 
+# International License.
+# 
+# Author: Marc Teves
+# 
+# CODE HISTORY
+# 
+# INFORMATION
+# File creation: February 5, 2019
+# Development group: Group 1 - Iskolivery
+# Client group: None
+# Purpose of the software: To create a crowdsourced courier service for the 
+# UP Community
+# This file should contain all the record creation needed to seed the database 
+# with its default values.
+# The data can then be loaded with the rails db:seed command (or created 
+# alongside the database with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# Character.create(name: 'Luke', movie: movies.first)
 
 Location.create(name: 'Palma Hall')
 Location.create(name: 'Melchor Hall')
