14a2520805881974eab2c00574d75cfc8afc57b6
Hakurei Reimu <gmike.travis@gmail.com>
2019-02-17 22:20:27 +0800

    wrote tests for expected behavior

--- iskolivery/test/controllers/sessions_controller_test.rb
+++ iskolivery/test/controllers/sessions_controller_test.rb
@@ -1,9 +1,35 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get login_path
-    assert_response :success
+	def setup
+		@user = users(:one)
+	end
+  test "root should route to login page" do
+    get root_path
+	assert_template 'sessions/new'
   end
+
+  test "login with invalid info" do
+	  visit_login_page
+	  post login_path, params: { session: { email: '', password: '' } }
+	  assert_template 'sessions/new'
+  end
+
+  test "login with valid info" do
+	  visit_login_page
+	  setup
+	  post login_path, params: { session: { email: @user.email,
+										 password: 'password' } }
+	  assert_redirected_to home_path
+  end
+
+  test "visiting login after already logged in" do
+  end
+
+  private
+	  def visit_login_page
+		get login_path
+		assert_template 'sessions/new'
+	  end
 
 end
