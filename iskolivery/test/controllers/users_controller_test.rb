da8089e0a7004d2c5148685879e173c42d7f48be
Hakurei Reimu <gmike.travis@gmail.com>
2019-02-21 22:32:11 +0800

    wrote tests for proper user index behavior

--- iskolivery/test/controllers/users_controller_test.rb
+++ iskolivery/test/controllers/users_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get users_home_url
-    assert_response :success
-  end
+	test "admin user view index" do
+		admin_user = users(:two)
+		post login_path, params: { session: { email: admin_user.email,
+											 password: 'password' } }
+		get '/users'
+		assert_response :success
+	end
+
+	test "regular user view index" do
+		regular_user = users(:one)
+		post login_path, params: { session: { email: regular_user.email,
+											 password: 'password' } }
+		get '/users'
+		assert_response 403
+	end
 
 end
