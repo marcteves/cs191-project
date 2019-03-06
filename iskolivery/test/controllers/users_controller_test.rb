456d9b5fcc55588dd6ae486b35eafe5212b9dbd0
Hakurei Reimu <gmike.travis@gmail.com>
2019-03-07 03:48:22 +0800

    fixed broken test

--- iskolivery/test/controllers/users_controller_test.rb
+++ iskolivery/test/controllers/users_controller_test.rb
@@ -9,28 +9,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 		assert_response :success
 	end
 
-	test "admin verify user" do 
+	test "admin verify toggle user" do 
 		admin_user = users(:two)
 		unverified_user = users(:three)
-		assert_equal false, unverified_user.verified
 
-		post login_path, params: { session: { email: admin_user.email,
-											 password: 'password' } }
-		post verify_path, params: { user_id: unverified_user.id }
+		assert_changes 'User.find_by(id: unverified_user.id).verified?',
+			from: false, to: true do
 
-		assert_equal true, unverified_user.verified
-	end
+			post login_path, params: { session: { email: admin_user.email,
+												 password: 'password' } }
+			post verify_path, params: { user_id: unverified_user.id }
+			assert_response :success
 
-	test "admin unverify user" do 
-		admin_user = users(:two)
-		unverified_user = users(:three)
-		assert_equal true, unverified_user.verified
+		end
 
-		post login_path, params: { session: { email: admin_user.email,
-											 password: 'password' } }
-		post verify_path, params: { user_id: unverified_user.id }
-
-		assert_equal false, unverified_user.verified
 	end
 
 	test "regular user view index" do
@@ -38,7 +30,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 		post login_path, params: { session: { email: regular_user.email,
 											 password: 'password' } }
 		get '/users'
-		assert_response 403
+		assert_response 422
 	end
 
 end
