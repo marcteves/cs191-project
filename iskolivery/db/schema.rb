95c9d2d9e1036fdcc06069170c7557b99f45c2d5
Hakurei Reimu <gmike.travis@gmail.com>
2019-03-07 03:02:23 +0800

    add verified field to user

--- iskolivery/db/schema.rb
+++ iskolivery/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190207155839) do
+ActiveRecord::Schema.define(version: 20190306184445) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer "requester_id"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20190207155839) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false, null: false
+    t.boolean "verified"
   end
 
 end
