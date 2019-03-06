5aafefac5cb726740547de4fd168a3a9d63ff13f
Hakurei Reimu <gmike.travis@gmail.com>
2019-03-07 03:00:24 +0800

    wrote tests

--- iskolivery/test/controllers/users_controller_test.rb
+++ iskolivery/test/controllers/users_controller_test.rb
@@ -9,6 +9,30 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 		assert_response :success
 	end
 
+	test "admin verify user" do 
+		admin_user = users(:two)
+		unverified_user = users(:three)
+		assert_equal false, unverified_user.verified
+
+		post login_path, params: { session: { email: admin_user.email,
+											 password: 'password' } }
+		post verify_path, params: { user_id: unverified_user.id }
+
+		assert_equal true, unverified_user.verified
+	end
+
+	test "admin unverify user" do 
+		admin_user = users(:two)
+		unverified_user = users(:three)
+		assert_equal true, unverified_user.verified
+
+		post login_path, params: { session: { email: admin_user.email,
+											 password: 'password' } }
+		post verify_path, params: { user_id: unverified_user.id }
+
+		assert_equal false, unverified_user.verified
+	end
+
 	test "regular user view index" do
 		regular_user = users(:one)
 		post login_path, params: { session: { email: regular_user.email,
