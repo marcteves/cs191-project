5ec87a1dd57e8d7d1bd8bb9e8cd6233c6744da6f
Hakurei Reimu <gmike.travis@gmail.com>
2019-03-07 19:25:07 +0800

    created name field in users

--- iskolivery/db/schema.rb
+++ iskolivery/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190306184445) do
+ActiveRecord::Schema.define(version: 20190307104017) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer "requester_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20190306184445) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false, null: false
     t.boolean "verified", default: false, null: false
+    t.string "name"
   end
 
 end
