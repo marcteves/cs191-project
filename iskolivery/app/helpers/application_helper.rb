278947d4cf88398a77f4d44dde511016013f3ba8
Marc Teves <gmarc.teves@gmail.com>
2019-04-03 14:31:42 +0800

    added flash helper for semantic classes

--- iskolivery/app/helpers/application_helper.rb
+++ iskolivery/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def flash_class(level)
+		case level
+		when :success then "ui green message"
+		when :error then "ui red message"
+		when :notice then "ui orange message"
+		end
+	end
 end
