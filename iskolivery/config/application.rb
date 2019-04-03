8874af2200c8d4d7f22335a9651674a8c7259f3c
Marc Teves <gmarc.teves@gmail.com>
2019-04-03 15:39:43 +0800

    moved email regex to application level config and changed pattern

--- iskolivery/config/application.rb
+++ iskolivery/config/application.rb
@@ -11,6 +11,7 @@ module Iskolivery
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 	config.new_rating_weight = 0.05
+	config.email_regex = /\A[\w+\-.]+@up\.edu\.ph\z/i
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
